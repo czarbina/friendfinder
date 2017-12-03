39ff75cd2de7c19d37911a002f634d2fadefbcaa
Sabrina Kesler <sabrina@getvictorious.com>
2017-12-03 15:48:15 -0800

    adding things that aren't working yet

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friendsArray = [
+	{
+  "name":"Chopino",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/c/c8/ChaCha_Red_Logo.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}]
