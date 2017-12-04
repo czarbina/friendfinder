c6e0de733ea4aeb4267cec3545cc32ff4760117f
Sabrina Kesler <sabrina@getvictorious.com>
2017-12-03 17:01:26 -0800

    Returning json from correct route

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,4 +14,79 @@ var friendsArray = [
       4,
       1
     ]
-}]
+	},
+
+	{
+  "name":"Goro",
+  "photo":"https://i.imgur.com/T2IErvp.jpg",
+  "scores":[
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+    ]
+	},
+
+
+	{
+  "name":"Kevin",
+  "photo":"NO",
+  "scores":[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+	},
+
+
+	{
+  "name":"Sabrina",
+  "photo":"Nope",
+  "scores":[
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
+    ]
+	},
+
+
+	{
+  "name":"Drew Doughty",
+  "photo":"https://static.tumblr.com/a28cfa45c50c71e0051b8ae9b0b86c19/ex1yr2v/ex5nz0u2p/tumblr_static_tumblr_static_amlq9q3qdn48s0ow4wk008ss4_640.jpg",
+  "scores":[
+      1,
+      5,
+      1,
+      5,
+      1,
+      5,
+      1,
+      5,
+      1,
+      5
+    ]
+	}
+
+]
+
+module.exports = friendsArray;
