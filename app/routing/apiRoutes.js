b1b96a1e1076ad773d37a2a7b052936583e39f30
Sabrina Kesler <sabrina@getvictorious.com>
2017-12-03 17:38:03 -0800

    Posting new friends successful

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,58 +1,34 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+// Requiring data source which we are storing in a file with an array of "friends."
 
 var friendsData = require("../data/friends");
-
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
+// API GET Requests for friends JSON. We designate /api/friends to be an
+// endpoint that will deliver our friendsArray JSON when visited.
 
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
+  // Below code handles when a user submits the survey.
   // In each of the below cases, when a user submits form data (a JSON object)
   // ...the JSON is pushed to the appropriate JavaScript array
   // (ex. User fills out a reservation request... this data is then sent to the server...
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
+  app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
+      friendsData.push(req.body);
       res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
+      console.log(friendsArray);
+    
   });
 };
