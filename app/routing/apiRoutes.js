40839d6a5f725116a1a43585f25ad60ea251afa6
Sabrina Kesler <sabrina@getvictorious.com>
2017-12-04 21:05:35 -0800

    Added comments that were necessary and removed unnecessary code comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,4 @@
-// Requiring data source which we are storing in a file with an array of "friends."
-
 var friendsData = require("../data/friends");
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 
 module.exports = function(app) {
 // API GET Requests for friends JSON. We designate /api/friends to be an
@@ -15,9 +8,9 @@ module.exports = function(app) {
     res.json(friendsData);
   });
 
-  // API POST Request
-  // Below code handles when a user submits the survey.
-  // ========================================================
+// API POST Request
+// Below code handles when a user submits the survey.
+// ========================================================
 
   app.post("/api/friends", function(req, res) {
 
@@ -25,59 +18,29 @@ module.exports = function(app) {
   // ========================================================
     friendsData.push(req.body);
 
-  // Below code takes sum of req.body score
+  // Below code will allow us to sum arrays of numbers for comparison later
   // ========================================================
-    // var myScores = req.body.scores;
-  // console.log(myScores);
-
     var sum = function(total, num) {
       return parseInt(total) + parseInt(num); 
     }
-
-  // score of user lives in myTotal 
-    // var myTotal = myScores.reduce(sum);
-    // console.log(myTotal);
-    console.log(req.body.scores.reduce(sum));
-
       
-  // Loops through friend list to retrieve scores of friends
-  // friendTotal houses every score
- 
-    var friendScoreArray = [];  
+  // Loops through friends array to find matching sum scores and will respond
+  // with the closest match. NOTE: Logic far from perfect here, but I just
+  // wanted to get something. 
 
     for (i=0; i < friendsData.length; i++) {
       if(req.body.scores.reduce(sum) - friendsData[i].scores.reduce(sum) === 0) {
 
         console.log("You have a PERFECT match: " + friendsData[i].name);
-        // console.log(friendsData[i].name);
         res.json(friendsData[i]);
-
-
-        // console.log("Friend: " + friendsData[i].name +
-        //   "|| Score: " + friendsData[i].scores.reduce(sum));
-        // friendScoreArray.push(friendsData[i].scores.reduce(sum));
       }
 
       else if (req.body.scores.reduce(sum) - friendsData[i].scores.reduce(sum) === 1 ||
         req.body.scores.reduce(sum) - friendsData[i].scores.reduce(sum) === -1) {
+
         console.log("You have an IMPERFECT match: " + friendsData[i].name);
         res.json(friendsData[i]);
       }
-
     }  
-      // for(j=0; j<friendScoreArray.length;j++) {
-      //   if (myTotal === friendScoreArray[j]) {
-      //     console.log("We have a match");
-      //   }
-      //   else {
-      //     console.log("No match");
-      //   }
-      // }
-
-      // res.json(friendsData[0]);
-      // console.log(friendScoreArray)
-      // console.log(friendsData);
-
   });
-      
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,6 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
 var path = require("path");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
 
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
@@ -27,4 +14,5 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+  
 };
